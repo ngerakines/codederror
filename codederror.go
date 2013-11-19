2454a188530725b85b1556c92db62ea1205e982e
Nick Gerakines <nick@foundapp.com>
2013-11-19 18:33:12 -0500

    Implementing the Error interface.

--- codederror.go
+++ codederror.go
@@ -11,6 +11,17 @@ type CodedError struct {
     code uint16
 }
 
-func (ce CodedError) String() string {
+func (ce *CodedError) String() string {
     return fmt.Sprintf("%s%d", strings.Join(ce.namespaces, ""), ce.code)
+}
+
+func (ce *CodedError) Error() string {
+    return ce.String()
+}
+
+func NewCodedError(namespaces []string, code uint16) *CodedError {
+    ce := new(CodedError)
+    ce.namespaces = namespaces
+    ce.code = code
+    return ce
 }
--- codederror_test.go
+++ codederror_test.go
@@ -8,7 +8,7 @@ import(
 
 func TestSomething(t *testing.T) {
     Convey("CodedError types can be created", t, func() {
-        var ce = CodedError{[]string{"ABC", "DEF", "GHI"}, 14}
+        ce := NewCodedError([]string{"ABC", "DEF", "GHI"}, 14)
 	    So(ce.String(), ShouldEqual, "ABCDEFGHI14")
     })
 }
